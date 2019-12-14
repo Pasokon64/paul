aa865e64f6c62b20d5bcfbbaf3167d1a2b964690
Manoel Almeida <tmanoel64x@gmail.com>
2019-12-13 21:40:21 -0300

    Bonequinho animado

--- src/js/objects/Tamagotchi.js
+++ src/js/objects/Tamagotchi.js
@@ -2,11 +2,14 @@ class Tamagotchi {
 
     constructor(render) {
         this.render = render;
-        this.image = new Image();
-        this.image.src = './src/sprites/stage1/0.png';
+
+        this.animation = new SpriteAnimation(this.render);
+        this.animation.addFrame('./src/sprites/stage1/0.png');
+        this.animation.addFrame('./src/sprites/stage1/1.png');
+        this.animation.start();
     }
 
     draw() {
-        this.render.drawImage(this.image, 0, 0, 16, 16, 100, 100, 96, 96);
+        this.animation.drawAnimation();
     }
 }
